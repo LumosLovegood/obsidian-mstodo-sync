6eab57cef091f543a7a37acdebbaa8030f6d42cd
lumos <lovegoodlumos@gmail.com>
2022-10-28 22:41:09 +0800

    fix bugs

--- clientProvider.ts
+++ clientProvider.ts
@@ -1,21 +1,25 @@
 import * as msal from "@azure/msal-node";
+import * as msalCommon from "@azure/msal-common";
 import open from 'open';
-import fs from 'fs';
+import * as fs from 'fs';
 import { Client } from "@microsoft/microsoft-graph-client";
 import "isomorphic-fetch"; 
 
+/**
+ * @public
+ */
+export class ClientProvider {
+    static scopes:string[] = ['Tasks.ReadWrite','Calendars.ReadWrite','People.Read','Tasks.Read','openid', 'profile'];
+    private cachePath = "./cache/msal_cache.json";
+    pca: msal.PublicClientApplication;
 
-export default class ClientProvider {
-    static scopes = ['Tasks.ReadWrite','Calendars.ReadWrite','People.Read','Tasks.Read','openid', 'profile'];
-    cachePath = "./cache/msal_cache.json";
-
-    constructor(cliendId,authority){
-        const beforeCacheAccess = async  (cacheContext) => {
+    constructor(cliendId:string,authority:string){
+        const beforeCacheAccess = async  (cacheContext: msalCommon.TokenCacheContext) => {
             if(fs.existsSync(this.cachePath)){
                 cacheContext.tokenCache.deserialize(fs.readFileSync(this.cachePath, "utf-8"));
             }
         };        
-        const afterCacheAccess = async  (cacheContext) => {
+        const afterCacheAccess = async  (cacheContext: msalCommon.TokenCacheContext) => {
             if(cacheContext.cacheHasChanged){
                 fs.writeFile(this.cachePath, cacheContext.tokenCache.serialize(),function () {
                     console.log("write");
@@ -43,10 +47,10 @@ export default class ClientProvider {
         if(fs.existsSync(this.cachePath)){
             msalCacheManager.deserialize(fs.readFileSync(this.cachePath, "utf-8"));
         }
-        let accounts = await msalCacheManager.getAllAccounts();
+        const accounts = await msalCacheManager.getAllAccounts();
         if(accounts.length == 0){
             const deviceCodeRequest = {
-                deviceCodeCallback: (response) => {
+                deviceCodeCallback: (response: msalCommon.DeviceCodeResponse) => {
                     open(response["verificationUri"])
                     console.log(response['userCode'])
                 },
@@ -66,9 +70,9 @@ export default class ClientProvider {
         }
     }
     async getClient(){
-        const authProvider = async (callback) => {
-            let accessToken = await this.getAccessToken();
-            let error = " ";
+        const authProvider = async (callback: (arg0: string, arg1: string) => void) => {
+            const accessToken = await this.getAccessToken();
+            const error = " ";
             callback(error, accessToken);
         };
         return Client.init({
